78ed29b6d1cb84f59dcf10384514c4ef0066ac2a
Kashan-2912 <mkashan2912@gmail.com>
2026-01-01 18:35:11 +0500

    feat: C#2 - Chapter 01 - Created multi-step function using step arg

--- chapter01/src/index.ts
+++ chapter01/src/index.ts
@@ -23,6 +23,46 @@ const helloFunction = inngest.createFunction(
     }
 )
 
+console.log("FUNCTION CREATED: ", helloFunction.id());
+
+// multi-step function
+const multiStepFunction = inngest.createFunction(
+    { id: "multi-step-demo" },
+    { event: "demo/multi-step" },
+
+    // handle
+    async ({ event, step}) => {
+        // step - 01: First task...
+        const result1 = await step.run("step-1", async () => {
+            console.log("Ececuting Step-01 ...")
+            return { data: "Result from Step-01" };
+        });
+
+        console.log("STEP-01 Completed", result1);
+
+        // step - 02: Sleep for 5s...
+        await step.sleep("await-a-bit", "5s");
+        console.log("STEP-02 Completed");
+
+        // step - 03: Another task...
+        const result2 = await step.run("step-2", async () => {
+            console.log("Ececuting Step-02 ...")
+
+            // every current step has access to previous step results
+            return { data: "Result from Step-02", previous: result1 };
+        });
+
+        console.log("STEP-02 Completed", result2);
+
+        return {
+            message: "Multi-step workflow completed",
+            results: [result1, result2]
+        }
+    }
+)
+
+console.log("MULTISTEP FUNCTION CREATED: ", multiStepFunction.id());
+
 export const functions = [
     helloFunction
 ];
